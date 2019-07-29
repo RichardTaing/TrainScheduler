e0d524dcfba1754247e0ba1e4148e30705180f50
rst_979 <richard.taing@gmail.com>
2019-07-29 23:01:51 +1000

    updated html with link and script tags & psuedo coded js

---
+++ javascript/trainScheduler.js
@@ -0,0 +1,51 @@
+// Initialize Firebase
+
+//variable to store database name
+
+//Object to store entire firebase database as JSON object
+
+//variable to store key of object to update.
+
+//variable to hold input values
+
+//train object constructor
+
+//diplays initial current time
+
+//Updates 'current time', 'next arrival' and 'minutes away' on page every 1 minute
+
+//Creates a string with todays date and time of 'time';
+
+//Creates a new 'train' object from the user input values
+
+//Pushes "newTrain" object to firebase database.
+
+//Shows Add Train panel
+
+//Calculates and returns time of next train arrival.
+
+// Calulates and returns how many minutes away next train is.
+
+//Returns the difference in minutes bewteen trains next arrival and currrnt time.
+
+//Gets trains data from database, then used data to displays train stats on screen.
+
+//Creates a moment.js object from the 'first train time' value ('time' HH:mm) enterd by user
+//Creates a string storing today's date from monent() in YYYY-MM-DD format.
+//Returns a string with todays date and time of first train.
+
+//Hides 'submit' button, shows 'Add Train' panel 'update' and 'Close' buttons.
+
+//Populates input fields with current train data to update.
+
+////Changes panel title to back to 'Add Train'.
+
+//Updates train data to firebase.
+
+//Creates a string with todays date and time of 'time';
+//Creates a new 'train' object from the user input values
+
+//Tests if 'Train Name' value is empty.
+//Tests if 'Destination' value is empty.
+//Tests if "First Train Time" is valid.
+//Tests if "Frequency" is valid.
